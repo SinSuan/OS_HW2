2ce128c0a7462c44342fbb8be49da6ceb2e1416a
SinSuan <vicky0002@gmail.com>
2023-11-18 17:07:13 +0800

    完成ped

--- HW2.c
+++ HW2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 #define DEBUG 0
 #define MAX_LEN 1024
@@ -11,9 +12,8 @@ void isDefultAddr(char *cwd, char *username){
 
     char DefultAddr[MAX_LEN] = "/home/";
     strcat(DefultAddr, username);
-    int n = strcmp(DefultAddr, cwd);
 
-    if( n == 0){
+    if(strcmp(DefultAddr, cwd) == 0){
         strcpy(cwd, "~");
     }
 
@@ -36,12 +36,42 @@ void prompt(){
     return;
 }
 
+void removeHeadSpace( char *cmd ){
+
+    char *ptr = cmd;
+    while(isspace(*ptr)){
+        ptr++;
+    }
+    memmove(cmd,ptr,sizeof(ptr)+1);
+
+}
+
+void determineWork( char *cmd ){
+
+    removeHeadSpace(cmd);
+
+    if(strncmp(cmd, "export", 6)==0){
+
+    } else if(strncmp(cmd, "echo", 4)==0){
+
+    } else if(strncmp(cmd, "pwd", 3)==0){
+        char cwd[MAX_LEN];
+        getcwd(cwd, sizeof(cwd));
+        printf("%s\n", cwd);
+    } else if(strncmp(cmd, "cd",2)==0) {
+
+    }
+
+    return;
+}
+
 int main() {
     
     while(1){
         prompt();
         char cmd[MAX_LEN];
         fgets(cmd, sizeof(cmd), stdin);
+        determineWork(cmd);
     }
 
     return 0;
