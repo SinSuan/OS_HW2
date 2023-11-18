9a9417270f115e3c652177d56d4b13075da573fe
SinSuan <vicky0002@gmail.com>
2023-11-18 23:26:09 +0800

    delete determineWork

--- HW2.c
+++ HW2.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200112L //the functionality from the 2001 edition of the POSIX standard (IEEE Standard 1003.1-2001) is made available.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,11 +10,11 @@
 #define MAX_LEN 1024
 
 
-void isDefultAddr(char *cwd, char *username){
+void dealDefultAddr(char *cwd, char *username){
 
     char DefultAddr[MAX_LEN] = "/home/";
     strcat(DefultAddr, username);
-
+    
     if(strcmp(DefultAddr, cwd) == 0){
         strcpy(cwd, "~");
     }
@@ -30,7 +32,7 @@ void prompt(){
     gethostname(hostname, sizeof(hostname));
     getcwd(cwd, sizeof(cwd));
 
-    isDefultAddr(cwd, username);
+    dealDefultAddr(cwd, username);
     printf("%s@%s:%s$", username, hostname, cwd);
 
     return;
@@ -46,36 +48,41 @@ void removeHeadSpace( char *cmd ){
 
 }
 
-void determineWork( char *cmd ){
-
-    removeHeadSpace(cmd);
-
-    if(strncmp(cmd, "export", 6)==0){
-
-    } else if(strncmp(cmd, "echo", 4)==0){
-
-    } else if(strncmp(cmd, "pwd", 3)==0){
-        char cwd[MAX_LEN];
-        getcwd(cwd, sizeof(cwd));
-        printf("%s\n", cwd);
-    } else if(strncmp(cmd, "cd",2)==0) {
-
-    }
-
-    return;
-}
-
 int main() {
     
+    char cmd[MAX_LEN];
+    char cmd_arg[MAX_LEN];
     while(1){
+
         prompt();
-        char cmd[MAX_LEN];
-        fgets(cmd, sizeof(cmd), stdin);
+        scanf("%s",cmd);
+
         if(strncmp(cmd, "exit", 4)==0){
+
             break;
-        } else {
-            determineWork(cmd);
-        }      
+
+        } else if(strncmp(cmd, "pwd", 3)==0){
+
+            char cwd[MAX_LEN];
+            getcwd(cwd, sizeof(cwd));
+            printf("%s\n", cwd);
+
+        }else if(strncmp(cmd, "cd",2)==0) {
+
+            scanf("%s",cmd_arg);
+            if(strncmp(cmd_arg, "/", 1)==0){
+                if (chdir(cmd_arg) != 0) {
+                    printf("cd: %s: No such file or directory\n", cmd);
+                    }
+            }
+
+        } else if(strncmp(cmd, "export", 6)==0){
+
+        } else if(strncmp(cmd, "echo", 4)==0){
+
+        }
+        strcpy(cmd,"");
+        strcpy(cmd_arg,"");
     }
 
     return 0;
