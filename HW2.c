020fc24ff84e36f209292613cad61a16c2adf666
SinSuan <vicky0002@gmail.com>
2023-11-18 15:22:19 +0800

    完成路逕

---
+++ HW2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define DEBUG 0
+#define MAX_LEN 1024
+
+
+void isDefultAddr(char *cwd, char *username){
+    
+    char DefultAddr[MAX_LEN] = "/home/";
+    strcat(DefultAddr, username);
+    int n = strcmp(DefultAddr, cwd);
+
+    if( n == 0){
+        strcpy(cwd, "~");
+    }
+
+    return;
+}
+
+void prompt(){
+    
+    char *username;
+    char hostname[MAX_LEN];
+    char cwd[MAX_LEN];
+    
+    username = getenv("LOGNAME");
+    gethostname(hostname, sizeof(hostname));
+    getcwd(cwd, sizeof(cwd));
+
+    isDefultAddr(cwd, username);
+    printf("%s@%s:%s$\n", username, hostname, cwd);
+
+    return;
+}
+
+int main() {
+    
+    prompt();
+
+    return 0;
+}
